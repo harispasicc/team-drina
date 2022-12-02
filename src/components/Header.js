b58324852e3ac190fad80499a4293ff73f0145ec
Vasilije Vujovic <vasilijev7@gmail.com>
2022-12-03 00:36:42 +0100

    Made a header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "../assets/styles/header.css";
+
+//Bootstrap imports
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Navbar
+        id="header"
+        style={{
+          height: "80px",
+        }}
+        variant="lignt"
+      >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              width={200}
+              height={60}
+              src={require("C://Users/Vasilije/team-drina/src/assets/logo.png")}
+              alt="x"
+            />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#pricing">About Us</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./Header";
+
+//Bootstrap imports
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default Home;
