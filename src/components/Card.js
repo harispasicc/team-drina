73868574e5c965e2cb64ceca044b08dd11de00e3
zabec <denis.imsirovic@gmail.com>
2022-12-03 00:55:24 +0100

    card

---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "../assets/styles/card.css";
+import picture from "../assets/images/Max.jpg";
+
+const Card = ({ name = "Max", age = 3, legs = 4 }) => {
+  const [flip, setFlip] = useState(false);
+
+  return (
+    <div className="flip-card card">
+      <div
+        className={`flip-card-inner ${flip ? "flip" : ""}`}
+        onMouseEnter={() => {
+          setFlip(true);
+        }}
+        onMouseLeave={() => {
+          setFlip(false);
+        }}
+      >
+        <div className="flip-card-front">
+          <img src={picture} alt="Pet" style={{ width: "130px" }} />
+        </div>
+        <div className="flip-card-back">
+          <div>
+            <div>Name: {name}</div>
+            <div>Age: {age}</div>
+            <div>Legs: {legs}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
