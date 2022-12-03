8b0aae5c6020692e2241afeee534dca03784fb99
Haris Pasic <harispasic@ymail.com>
2022-12-03 11:48:10 +0100

    price-list-2

--- src/components/PriceList.js
+++ src/components/PriceList.js
@@ -11,14 +11,13 @@ function PriceList() {
                 <div className="card-123-body">
                   <div className="text-center p-3">
                     <h5 className="card-123-title">Basic</h5>
-                    <small>Individual</small>
                     <br></br>
-                    <span className="h2">$8</span>/month
+                    <span className="h2">$20</span>/year
                     <br></br>
                   </div>
                   <p className="card-123-text">
-                    Some quick example text to build on the card-123 title and
-                    make up the bulk of the card-123's content.
+                    The crematory with basic urn or burial with basic coffin
+                    with monthly maintance
                   </p>
                 </div>
                 <ul className="list-group list-group-flush">
@@ -33,7 +32,7 @@ function PriceList() {
                     >
                       <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>{" "}
-                    Cras justo odio
+                    Crematory
                   </li>
                   <li className="list-group-item">
                     <svg
@@ -46,7 +45,7 @@ function PriceList() {
                     >
                       <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>{" "}
-                    Dapibus ac facilisis in
+                    Burial
                   </li>
                   <li className="list-group-item">
                     <svg
@@ -59,7 +58,7 @@ function PriceList() {
                     >
                       <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>{" "}
-                    Vestibulum at eros
+                    monthly maintance
                   </li>
                 </ul>
 
@@ -78,14 +77,13 @@ function PriceList() {
                 <div className="card-123-body">
                   <div className="text-center p-3">
                     <h5 className="card-123-title">Standard</h5>
-                    <small>Small Business</small>
                     <br></br>
-                    <span className="h2">$20</span>/month
+                    <span className="h2">$50</span>/year
                     <br></br>
                   </div>
                   <p className="card-123-text">
-                    Some quick example text to build on the card-123 title and
-                    make up the bulk of the card-123's content.
+                    The crematory with standard urn or burial with standard
+                    coffin with weekly maintance
                   </p>
                 </div>
                 <ul className="list-group list-group-flush">
@@ -100,7 +98,7 @@ function PriceList() {
                     >
                       <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>{" "}
-                    Cras justo odio
+                    Crematory
                   </li>
                   <li className="list-group-item">
                     <svg
@@ -113,7 +111,7 @@ function PriceList() {
                     >
                       <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>{" "}
-                    Dapibus ac facilisis in
+                    Burial
                   </li>
                   <li className="list-group-item">
                     <svg
@@ -126,7 +124,7 @@ function PriceList() {
                     >
                       <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>{" "}
-                    Vestibulum at eros
+                    Weekly maintance
                   </li>
                 </ul>
                 <div className="card-123-body text-center">
@@ -144,14 +142,13 @@ function PriceList() {
                 <div className="card-123-body">
                   <div className="text-center p-3">
                     <h5 className="card-123-title">Premium</h5>
-                    <small>Large Companies</small>
                     <br></br>
-                    <span className="h2">$40</span>/month
+                    <span className="h2">$80</span>/year
                     <br></br>
                   </div>
                   <p className="card-123-text">
-                    Some quick example text to build on the card-123 title and
-                    make up the bulk of the card-123's content.
+                    The crematory with premium Swarowski urn or burial with
+                    premium coffin with weekly maintance
                   </p>
                 </div>
                 <ul className="list-group list-group-flush">
