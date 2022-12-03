b92e20c2db9889d7d8278a4367658e9654727d6b
zabec <denis.imsirovic@gmail.com>
2022-12-03 01:41:58 +0100

    counter

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/styles/about-us.css";
 import logo from "../assets/images/logo.png";
 
 function AboutUs() {
+  const [pets, setPets] = useState(1234);
+  useEffect(() => {
+    let interval = setInterval(() => {
+      const newPets = Math.round(Math.random() * 5);
+      setPets((pets) => pets + newPets);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div>
       <h2 className="text-center text-primary">About Us</h2>
@@ -10,8 +22,9 @@ function AboutUs() {
       <div className="text-secondary fs-5 fw-semibold about-text">
         We are your partner in distress. Pets are our family members and we
         treat them with respect. In dark hours of parting our ways, we provide
-        support organizing appropriate funeral. Fill form or contact us for more
-        information.
+        support organizing appropriate funeral. <br />
+        Fill form or contact us for more information. <br />
+        <b>{pets}</b> pets accompanied to light.
       </div>
     </div>
   );
