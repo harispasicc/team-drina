ff4b62d3369003916dfee5acacc8bd47564da5e0
zabec <denis.imsirovic@gmail.com>
2022-12-03 01:08:57 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Footer />
     </div>
   );
 }
