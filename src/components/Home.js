9877b2746a55cf4f96e25cd66335ebb8e09ead98
Vasilije Vujovic <vasilijev7@gmail.com>
2022-12-03 02:07:43 +0100

    Added an animated background

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <Header />
+      <video autoPlay muted loop id="myVideo">
+        <source
+          src="https://static.videezy.com/system/resources/previews/000/038/011/original/abstract_line4.mp4"
+          type="video/mp4"
+        />
+      </video>
     </div>
   );
 }
